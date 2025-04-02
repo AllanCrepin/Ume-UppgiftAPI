52fdee7ae230eb8ed988584880e0944cf40b986d
Allan Crépin <alllan2107@gmail.com>
2025-04-02 10:20:14 +0200

    Add project files.

---
+++ UmeåUppgiftAPI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace UmeåUppgiftAPI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
